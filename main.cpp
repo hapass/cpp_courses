c424fb84c53e8e0e7c70a77bc1de2224a779f980
Pavelza <hapass@ya.ru>
2019-01-16 15:56:16 +0300

    File input.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 void sort(int* arr, int size, bool (*compare)(int, int)) {
     for(int i = 0; i < size; i++) {
@@ -13,12 +14,32 @@ void sort(int* arr, int size, bool (*compare)(int, int)) {
     }
 }
 
-int main() {
+void test_sort() {
     int arr[] { 4, 2, 3, 5, 6, 1 };
     int size = sizeof(arr)/sizeof(int);
     sort(arr, size, [](int a, int b) -> bool { return a > b; });
 
     for(int i = 0; i < size; i++) {
-        std::cout << arr[i] << std::endl;
+        printf("%d \n", arr[i]);
     }
+}
+
+void test_file() {
+    auto pFile = fopen("data.txt", "r");
+
+    if(pFile) {
+        std::cout << "file opened" << std::endl;
+
+        char str[1000];
+
+        fgets(str, 1000, pFile);
+        printf("%s\n", str);
+
+        fclose(pFile);
+    }
+}
+
+int main() {
+    test_sort();
+    test_file();
 }
