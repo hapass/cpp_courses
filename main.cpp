3bb1253f14ed03968476ff026fc6f0ffbd77d232
Pavelza <hapass@ya.ru>
2019-01-17 13:26:59 +0300

    More practice tasks.

--- main.cpp
+++ main.cpp
@@ -21,6 +21,11 @@ int main() {
 
     //practice day 2
     //-------------
+    // std::vector<int> integers { 1, 2, 3, 4, 5 };
+    // test_3(integers);
+    // std::vector<float> floats { 1.1, 2.2, 3.3, 4.4, 5.5 };
+    // test_3(floats);
+    // test_4();
     // test_5({ 1, 1000, 1, 1 });
     // test_5({ 1, 1000, 100, 2 });
     // test_5({ 1, 1, 1, 1000 });
--- practice_2.hpp
+++ practice_2.hpp
@@ -2,7 +2,62 @@
 
 #include <vector>
 #include <iostream>
+#include <fstream>
 
+/*
+    Override << operator for structure containing mobile phone number.
+*/
+
+
+/*
+    Find a sum of elements in a list.
+    Method should be templated for use with float and int.
+*/
+template<class T>
+void test_3(std::vector<T> list) {
+    T sum = 0;
+    for(auto i = list.begin(); i < list.end(); i++) {
+        sum += *i;
+    }
+    std::cout << sum << "\n";
+}
+
+/*
+    Read a list of pairs from file. Pairs containt city name and distance to the city from current location.
+    Find city name by distance and display it.
+    Use pair and vector.
+*/
+void test_4() {
+    int targetDistance;
+    std::cin >> targetDistance;
+
+    std::ifstream cities;
+    cities.open("data.txt");
+    std::vector<std::pair<int, std::string>> list;
+    if(cities.is_open()) {
+        std::string name;
+        int distance;
+        while(cities >> name >> distance) {
+            std::pair<int, std::string> pair(distance, name);
+            list.push_back(pair);
+        }
+    }
+
+    auto iterator = std::find_if(list.begin(), list.end(), [&](std::pair<int, std::string>& p){ return p.first == targetDistance; });
+    
+    if(iterator != list.end()) {
+        std::cout << iterator->second << '\n';
+    } else {
+        std::cout << "nothing found" << '\n';
+    }
+}
+
+/*
+    Given a vector of numbers. Remove one by one numbers, that:
+     * have left and right neighbours
+     * and a magnitude of twice the average of the element itself and its neighbours.
+    Use STL.
+*/
 void test_5(std::vector<int> vec) {
     int index = 1;
     while(index < vec.size() - 1) {
@@ -20,6 +75,10 @@ void test_5(std::vector<int> vec) {
     std::cout << "--------------------" << std::endl;
 }
 
+/*
+    Read a string from file and revert it.
+    Use STL.
+*/
 void test_6() {
     std::string str;
     std::cin >> str;
