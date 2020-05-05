cb4c690e42b5fb69a16f5a758d4faf2cef9779d9
Pavel Zavoiskikh <hapass@ya.ru>
2020-05-05 21:22:50 +0300

    Palindrome recursive naive implementation.

---
+++ yandex/white_belt/palindrome/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsPalindrom(string value) {
+    cout << value << endl;
+    if (value.length() <= 1) {
+        return true;
+    }
+
+    return value[0] == value[value.length() - 1] && IsPalindrom(value.substr(1, value.length() - 2));
+}
+
+int main() {
+    string value;
+    cin >> value;
+    cout << boolalpha << IsPalindrom(value) << endl;
+    return 0;
+}
