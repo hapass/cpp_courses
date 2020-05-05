cb57fceafc9f641a979c75d2a3c004c2f2f26722
Pavel Zavoiskikh <hapass@ya.ru>
2020-05-05 22:19:51 +0300

    Palindrome filter.

---
+++ yandex/white_belt/palindrome_filter/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> PalindromeFilter(vector<string> words, int minLength) {
+    vector<string> result;
+    for (const string& word : words) {
+        if (word.size() < minLength) {
+            continue;
+        }
+
+        bool isPalindrome = true;
+        for (int i = 0; i < word.size() / 2; i++) {
+            if (word[i] != word[word.size() - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            result.push_back(word);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    {
+        int minLength = 5;
+        vector<string> words = {"abacaba", "aba"};
+        vector<string> output = PalindromeFilter(words, minLength);
+        for (const string& word : output)
+        {
+            cout << word << endl;
+        }
+    }
+    cout << "_________" << endl;
+    {
+        int minLength = 2;
+        vector<string> words = { "abacaba", "aba" };
+        vector<string> output = PalindromeFilter(words, minLength);
+        for (const string& word : output)
+        {
+            cout << word << endl;
+        }
+    }
+    cout << "_________" << endl;
+    {
+        int minLength = 4;
+        vector<string> words = { "weew", "bro", "code" };
+        vector<string> output = PalindromeFilter(words, minLength);
+        for (const string& word : output)
+        {
+            cout << word << endl;
+        }
+    }
+    cout << "_________" << endl;
+    {
+        int minLength = 0;
+        vector<string> words = { "madam", "gentleman", "x", "abba", "" };
+        vector<string> output = PalindromeFilter(words, minLength);
+        for (const string& word : output)
+        {
+            cout << word << endl;
+        }
+    }
+    cout << "_________" << endl;
+    return 0;
+}
