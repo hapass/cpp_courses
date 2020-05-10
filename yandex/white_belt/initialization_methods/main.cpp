b2dce2eb56f3dc345df97619de87bf74d4a44967
Pavel Zavoiskikh <hapass@ya.ru>
2020-05-10 18:45:26 +0300

    Initialization methods.

---
+++ yandex/white_belt/initialization_methods/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct Incognizable {
+    int a = 0;
+    int b = 0;
+};
+
+int main() {
+    Incognizable a;
+    Incognizable b = {};
+    Incognizable c = {0};
+    Incognizable d = {0, 1};
+    return 0;
+}
