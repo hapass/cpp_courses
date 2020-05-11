90607d019efa244a1a01d7267e09d116349e48b9
Pavel Zavoiskikh <hapass@ya.ru>
2020-05-12 02:55:37 +0300

    Map set vector for rationals.

--- yandex/white_belt/rational_5/main.cpp
+++ yandex/white_belt/rational_5/main.cpp
@@ -2,6 +2,9 @@
 #include <cassert>
 #include <cmath>
 #include <sstream>
+#include <map>
+#include <set>
+#include <vector>
 
 using namespace std;
 
@@ -85,43 +88,38 @@ istream& operator>>(istream& stream, Rational& number) {
     return stream;
 }
 
+bool operator<(const Rational& lhs, const Rational& rhs) {
+    return rhs.Denominator() * lhs.Numerator() < lhs.Denominator() * rhs.Numerator();
+}
+
 int main() {
     {
-        ostringstream output;
-        output << Rational(-6, 8);
-        if (output.str() != "-3/4") {
-            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
+        const set<Rational> rs = {{1, 2}, {1, 25}, {3, 4}, {3, 4}, {1, 2}};
+        if (rs.size() != 3) {
+            cout << "Wrong amount of items in the set" << endl;
             return 1;
         }
-    }
 
-    {
-        istringstream input("5/7");
-        Rational r;
-        input >> r;
-        bool equal = r == Rational(5, 7);
-        if (!equal) {
-            cout << "5/7 is incorrectly read as " << r << endl;
+        vector<Rational> v;
+        for (auto x : rs) {
+            v.push_back(x);
+        }
+        if (v != vector<Rational>{{1, 25}, {1, 2}, {3, 4}}) {
+            cout << "Rationals comparison works incorrectly" << endl;
             return 2;
         }
     }
 
     {
-        istringstream input("5/7 10/8");
-        Rational r1, r2;
-        input >> r1 >> r2;
-        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
-            return 3;
-        }
+        map<Rational, int> count;
+        ++count[{1, 2}];
+        ++count[{1, 2}];
 
-        input >> r1;
-        input >> r2;
-        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
-            return 4;
+        ++count[{2, 3}];
+
+        if (count.size() != 2) {
+            cout << "Wrong amount of items in the map" << endl;
+            return 3;
         }
     }
 
